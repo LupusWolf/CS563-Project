485888c4ff2266e47332762d6fa412540d39d9a0
Matthew Cole <matthewcole66@gmail.com>
2023-03-14 00:07:57 -0700

    Added blue theming to the site
    Added blue theming and generated a color palate. Made buttons rounder. Added background to text.

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ function setupTransition(classname)
 function setAbout()
 {
     setupTransition("about");
-    left.innerHTML = 'Hello, I am Matthew Cole (He/Him). I am currently pursuing a Masters in Computer Science. I am generally interested in software engineering, but I am not particularly specialized within a given field. I have been programming since I was young and have coded in a wide variety of languages, but I am most comfortable in C#, Python, and C++. I generally am very flexible when it comes to languages since I have worked with so many and I am able to pick up languages relatively quickly. However, I would say that I find python to be my most comfortable language for very small programs and I find C# to be my most comfortable language for larger programs. I don\'t have really any projects of note which is a huge problem that I know I need to work on. Building up a portfolio is a big part of what a potential employer is going to look for and that is what I would consider my biggest current weakness.';
+    var textNode = document.createElement('p');
+    textNode.className = "textbox";
+    textNode.innerText = 'Hello, I am Matthew Cole (He/Him). I am currently pursuing a Masters in Computer Science. I am generally interested in software engineering, but I am not particularly specialized within a given field. I have been programming since I was young and have coded in a wide variety of languages, but I am most comfortable in C#, Python, and C++. I generally am very flexible when it comes to languages since I have worked with so many and I am able to pick up languages relatively quickly. However, I would say that I find python to be my most comfortable language for very small programs and I find C# to be my most comfortable language for larger programs. I don\'t have really any projects of note which is a huge problem that I know I need to work on. Building up a portfolio is a big part of what a potential employer is going to look for and that is what I would consider my biggest current weakness.';
+    left.appendChild(textNode);
     var image = document.createElement("img");
     image.src = 'matthew.jpg';
     image.width = 400;
@@ -43,16 +46,26 @@ function createListElement(title, elements)
 
 function setPreviousWork()
 {
-    setupTransition("work");
+    setupTransition("work textbox");
     left.appendChild(createListElement("Classes", ["CS566","CS563","CS510Java"]));
-    right.appendChild(createListElement("Languages", ["C#","C++","Python","Java"]));
+    right.appendChild(createListElement("Languages", ["C++","Python","Java", "SQL","C#"]));
 }
 document.querySelector("#work").addEventListener("click", setPreviousWork);
 
+function setOneProject(node, title, description)
+{
+    var childNode = document.createElement("p");
+    childNode.innerHTML = "<h1>" + title + "</h1>";
+    node.append(childNode);
+    childNode = document.createElement("p");
+    childNode.innerText = description;
+    node.append(childNode);
+}
 function setProjects()
 {
     setupTransition("projects");
-    left.innerHTML = 'Projects';
+    setOneProject(left, "Left Title","Left Description")
+    setOneProject(right, "Right Title","Right Description")
 }
 document.querySelector("#projects").addEventListener("click", setProjects);
 
